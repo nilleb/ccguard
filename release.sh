4f8435e38cffabc1e9a0608a6e51a6e73acc4e82
Ivo Bellin Salarin <ivo@lumapps.com>
2020-01-13 22:18:20 +0100

    feat: a release script
    (credits go to Alexandre Conrad)

---
+++ release.sh
@@ -0,0 +1,14 @@
+# Before you release
+# - install pandoc on your system: apt-get install pandoc
+# - install pypandoc: pip install pypandoc
+# - install twine: pip install twine
+# - bump package version in setup.py
+# - update the package version in the CHANGES file
+
+PKG_NAME=$(python setup.py --name)
+PKG_VERSION=$(python setup.py --version)
+
+git tag -am "release v${PKG_VERSION}" v${PKG_VERSION}
+git push --tags
+python setup.py sdist
+twine upload dist/${PKG_NAME}-${PKG_VERSION}.tar.gz
