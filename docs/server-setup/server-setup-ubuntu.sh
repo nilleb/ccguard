de52baa37dc7fa0f77322336bdc9a374787326db
Ivo Bellin Salarin <ivo@lumapps.com>
2020-02-08 19:03:39 +0100

    fix: setup scripts

--- docs/server-setup/server-setup-ubuntu.sh
+++ docs/server-setup/server-setup-ubuntu.sh
@@ -7,32 +7,32 @@ TOKEN=secret
 use_ssl="true"
 
 sudo apt update
-sudo apt install nginx
-sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
-sudo apt install python3-venv
+sudo apt -y install nginx python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools python3-venv
 
-curl https://github.com/nilleb/ccguard/tree/master/docs/server-setup/ccguard.service --output ccguard.service.template
-curl https://github.com/nilleb/ccguard/tree/master/docs/server-setup/nginx-ccguard --output nginx-ccguard.template
+curl -L https://raw.githubusercontent.com/nilleb/ccguard/master/docs/server-setup/ccguard.service --output ccguard.service.template
+curl -L https://raw.githubusercontent.com/nilleb/ccguard/master/docs/server-setup/nginx-ccguard --output nginx-ccguard.template
 
 mkdir $HOME/letsencrypt
 
+curl -L https://raw.githubusercontent.com/nilleb/ccguard/master/docs/server-setup/envsubst_surrogate.cfg --output envsubst_surrogate.cfg
+
 . envsubst_surrogate.cfg
 printf "%s\n" "$(apply_shell_expansion ccguard.service.template)" > ccguard.service
-printf "%s\n" "$(apply_shell_expansion cnginx-ccguard.template)" > nginx-ccguard
+printf "%s\n" "$(apply_shell_expansion nginx-ccguard.template)" > nginx-ccguard
 
 # gunicorn setup
-python3 -m venv env
+python3 -m venv env --system-site-packages
 source env/bin/activate
 pip install ccguard gunicorn flask
 # TODO: eval the variables in ccguard.service
-mv ccguard.service /etc/systemd/system/ccguard.service
+sudo mv ccguard.service /etc/systemd/system/ccguard.service
 sudo systemctl start ccguard
 sudo systemctl enable ccguard
 sudo systemctl status ccguard
 
 # nginx setup
 # TODO: eval the variables in nginx-ccguard
-mv nginx-ccguard /etc/nginx/sites-available/ccguard
+sudo mv nginx-ccguard /etc/nginx/sites-available/ccguard
 sudo ln -s /etc/nginx/sites-available/ccguard /etc/nginx/sites-enabled
 sudo nginx -t
 sudo systemctl restart nginx
