7022a4687d42a980c5984c18250c836a58cbb464
Ivo Bellin Salarin <ivo@lumapps.com>
2020-03-01 16:08:47 +0100

    feat: the telemetry

--- ccguard/ccguard_server.py
+++ ccguard/ccguard_server.py
@@ -1,9 +1,15 @@
 import argparse
+import datetime
+import hashlib
 import io
 import logging
+import socket
+import sqlite3
+import threading
 
 import flask
 import lxml
+import requests
 from flask import abort, jsonify, request
 from pycobertura import Cobertura
 from pycobertura.reporters import HtmlReporter, HtmlReporterDelta
@@ -54,6 +60,118 @@ def home():
     )
 
 
+class SqliteServerAdapter(object):
+    def __init__(self, config):
+        config = config or ccguard.configuration()
+        dbpath = str(config.get("sqlite.dbpath"))
+        logging.debug(dbpath)
+        self.conn = sqlite3.connect(dbpath)
+        self._create_table()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.conn.close()
+
+    def _create_table(self):
+        statement = (
+            "CREATE TABLE IF NOT EXISTS `ccguard_server_stats` ("
+            "`ip` varchar(40) NOT NULL, "
+            "`collected_at` ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP, "
+            "`repositories_count` INT DEFAULT 0, "
+            "`commits_count` INT DEFAULT 0, "
+            "`last_updated` ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP, "
+            "PRIMARY KEY  (`ip`) );"
+        )
+        self.conn.execute(statement)
+
+    def record(self, data: dict):
+        if not data or not isinstance(data, dict) or "ip" not in data:
+            raise ValueError("Unwilling to persist invalid data.")
+
+        statement = (
+            "INSERT INTO ccguard_server_stats "
+            "(ip, repositories_count, commits_count) VALUES (?, ?, ?)"
+        )
+        data_tuple = (
+            data["ip"],
+            data.get("repositories_count", 0),
+            data.get("commits_count", 0),
+        )
+        try:
+            self.conn.execute(statement, data_tuple)
+            self.conn.commit()
+        except sqlite3.IntegrityError:
+            logging.exception("This IP seems to have already been recorded.")
+            statement = (
+                "UPDATE ccguard_server_stats "
+                f'SET repositories_count = {data["repositories_count"]}, '
+                f'commits_count = {data["commits_count"]}, '
+                f'last_updated = "{datetime.datetime.now()}" '
+                f'WHERE ip = "{data["ip"]}"'
+            )
+            logging.debug(statement)
+            self.conn.execute(statement)
+            self.conn.commit()
+
+    def totals(self) -> dict:
+        query = (
+            "SELECT count(*), sum(repositories_count), sum(commits_count) "
+            "FROM ccguard_server_stats"
+        )
+        rows = self.conn.execute(query).fetchall()
+
+        data = {
+            "servers": rows[0][0],
+            "served_repositories": rows[0][1] or 0,
+            "recorded_commits": rows[0][2] or 0,
+        }
+
+        return data
+
+    def list_repositories(self) -> frozenset:
+        query = (
+            "SELECT name FROM sqlite_master "
+            'WHERE type = "table" AND name LIKE "timestamped_coverage_%"'
+        )
+
+        tuples = self.conn.execute(query).fetchall()
+
+        prefix_len = len("timestamped_coverage_")
+        return frozenset({row[0][prefix_len:] for row in tuples})
+
+    def commits_count(self, repository_id) -> frozenset:
+        query = "SELECT count(*) FROM timestamped_coverage_{repository_id} ".format(
+            repository_id=repository_id
+        )
+        tuples = self.conn.execute(query).fetchone()
+        return next(iter(tuples))
+
+
+def record_telemetry_event(data: dict, remote_addr: str, config: dict = None):
+    ip_bytes = socket.inet_aton(remote_addr)
+    data["ip"] = hashlib.sha224(ip_bytes).hexdigest()
+    with SqliteServerAdapter(config) as adapter:
+        adapter.record(data)
+
+
+@app.route("/api/v1/telemetry", methods=["POST"])
+@authenticated
+def api_telemetry_collect():
+    data = request.get_json()
+    record_telemetry_event(data, remote_addr=request.remote_addr)
+    return "OK, Thanks!"
+
+
+@app.route("/api/v1/telemetry", methods=["GET"])
+@authenticated
+def api_telemetry_get():
+    with SqliteServerAdapter() as server_adapter:
+        data = server_adapter.totals()
+    return jsonify(data)
+
+
 def api_repositories_debug_common():
     config = ccguard.configuration()
     adapter_class = ccguard.adapter_factory(None, config)
@@ -87,7 +205,7 @@ def api_references_all_v1(repository_id):
     return jsonify(list(commits))
 
 
-@app.route("/api/v2/references/<string:repository_id>/all", methods=["GET"])
+@app.route("/api/v1/references/<string:repository_id>/all", methods=["GET"])
 def api_references_all_v2(repository_id):
     commits = api_references_all_common(repository_id)
     return jsonify({"references": list(commits)})
@@ -233,12 +351,47 @@ def parse_args(args=None):
     return parser.parse_args(args)
 
 
+def _prepare_event(config=None):
+    with SqliteServerAdapter(config) as adapter:
+        repositories = adapter.list_repositories()
+
+        data = {"repositories_count": len(repositories), "commits_count": 0}
+
+        for repository_id in repositories:
+            commits = adapter.commits_count(repository_id)
+            data["commits_count"] += commits
+
+    return data
+
+
+def send_telemetry_event(config: dict = None):
+    config = config or ccguard.configuration()
+
+    data = _prepare_event(config)
+
+    if config.get("telemetry.disable", False):
+        logging.debug("telemetry: disabled")
+        record_telemetry_event(data, "127.0.0.1", config=config)
+    else:
+        logging.debug("telemetry: enabled")
+        x = threading.Thread(
+            target=requests.post,
+            args=("https://ccguard.nilleb.com/api/v1/telemetry",),
+            kwargs={"data": data},
+        )
+        x.start()
+
+    return data
+
+
 def load_app(token):
+    send_telemetry_event()
     app.config["TOKEN"] = token
     return app
 
 
 def main(args=None, app=app):
+    send_telemetry_event()
     logging.basicConfig(level=logging.DEBUG)
     args = parse_args(args)
     app.config["TOKEN"] = args.token
