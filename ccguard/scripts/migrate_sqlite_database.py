2d2211cf9e288dc59bc57b8cbf2a1f5c2fd9d1bb
Ivo Bellin Salarin <ivo@lumapps.com>
2020-03-05 20:51:54 +0100

    feat(migration): populate the line rate

--- ccguard/scripts/migrate_sqlite_database.py
+++ ccguard/scripts/migrate_sqlite_database.py
@@ -1,10 +1,55 @@
 import argparse
 import sqlite3
+import lxml.etree as ET
 
 
-def migrate_03_04(dbpath):
-    print("Migrating database {}".format(dbpath))
-    conn = sqlite3.connect(dbpath)
+def _get_line_coverage(data: bytes) -> (float, int, int):
+    try:
+        tree = ET.fromstring(data)
+        return {
+            "line_rate": float(tree.get("line-rate", 0.0)),
+            "lines_covered": int(tree.get("lines-covered", 0)),
+            "lines_valid": int(tree.get("lines-valid", 0)),
+        }
+    except ET.XMLSyntaxError:
+        return 0.0, 0, 0
+
+
+def _alter_table(conn, new_columns, table, sql):
+    for name, ctype, default in new_columns:
+        if name not in sql:
+            print("Adding {} {} DEFAULT {}".format(name, ctype, default))
+            ddl = "ALTER TABLE {} ADD COLUMN `{}` {} DEFAULT {}".format(
+                table, name, ctype, default
+            )
+            conn.execute(ddl)
+
+
+def _update_data(conn, table):
+    query = "SELECT commit_id, coverage_data " "FROM {} where line_rate = 0.0".format(
+        table
+    )
+    rows = conn.execute(query).fetchall()
+    for row in rows:
+        commit, data = row
+        data_tuple = _get_line_coverage(data)
+        update = (
+            "UPDATE {table} "
+            "SET line_rate = {line_rate}, "
+            "lines_covered = {lines_covered}, "
+            "lines_valid = {lines_valid} "
+            'WHERE commit_id = "{commit}";'
+        ).format(**data_tuple, table=table, commit=commit)
+
+        print(
+            "{} {line_rate} {lines_covered} {lines_valid}".format(commit, **data_tuple)
+        )
+        cursor = conn.execute(update)
+        conn.commit()
+        cursor.close()
+
+
+def migration_steps_03_04(conn):
     all_repositories = (
         "SELECT name, sql FROM sqlite_master "
         'WHERE type="table" AND name LIKE "timestamped_coverage_%"'
@@ -16,17 +61,24 @@ def migrate_03_04(dbpath):
         ("count", "INT", "1"),
         ("line_rate", "REAL", "0.0"),
         ("lts", "INTEGER", "0"),
+        ("lines_covered", "INTEGER", "0"),
+        ("lines_valid", "INTEGER", "0"),
     ]
+
     for table, sql in tables:
         print("Processing table {}".format(table))
-        for name, ctype, default in new_columns:
-            if name not in sql:
-                print("Adding {} {} DEFAULT {}".format(name, ctype, default))
-                ddl = "ALTER TABLE {} ADD COLUMN `{}` {} DEFAULT {}".format(
-                    table, name, ctype, default
-                )
-                conn.execute(ddl)
-    conn.commit()
+        _alter_table(conn, new_columns, table, sql)
+        _update_data(conn, table)
+        conn.commit()
+
+
+def migrate_03_04(dbpath):
+    print("Migrating database {}".format(dbpath))
+    try:
+        conn = sqlite3.connect(dbpath)
+        migration_steps_03_04(conn)
+    finally:
+        conn.close()
 
 
 if __name__ == "__main__":
