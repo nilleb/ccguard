e73e5909ed60866316f9d75561f6049dbd31db68
Ivo Bellin Salarin <ivo@lumapps.com>
2020-04-14 20:59:21 +0200

    chore: bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ccguard",
-    version="0.5.1",
+    version="0.5.2",
     entry_points={
         "console_scripts": [
             "ccguard=ccguard.ccguard:main",
