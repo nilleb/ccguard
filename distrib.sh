c0cf1cf9549e562afb101679e06851f0e2960126
Ivo Bellin Salarin <ivo@lumapps.com>
2020-04-14 20:59:48 +0200

    chore: upgrade version in all the distrib scripts

--- distrib.sh
+++ distrib.sh
@@ -1,3 +1,9 @@
+rm dist/*.whl
+
+PKG_NAME=$(python setup.py --name)
+PKG_VERSION=$(python setup.py --version)
+sed -i "" "s/\(__version__ = \"\).*\(\"\)/\1${PKG_VERSION}\2/g" ccguard/__init__.py
+
 python3 -m venv env
 source env/bin/activate
 # install python packaging tools
