65b926465db85357307d3a0a2a99ec1fbd2dd96d
Ivo Bellin Salarin <ivo@lumapps.com>
2020-04-16 22:13:00 +0200

    feat(convert): exclude files from go report

--- ccguard/test_ccguard_convert.py
+++ ccguard/test_ccguard_convert.py
@@ -10,3 +10,20 @@ def test_convert_go_xml():
     assert os.path.exists(outpath)
     xml = ET.parse(outpath).getroot()
     assert float(xml.get("line-rate"))
+
+
+def test_exclusions():
+    path = "ccguard/test_data/convert/go-coverage.txt"
+    (
+        report,
+        rate,
+        covered,
+        total,
+        packages,
+        packages_coverage,
+        files,
+        file_coverage,
+    ) = ccguard_convert.convert_golang_report(
+        path, config={"convert.exclusions.go": ["**/config/default.go"]}
+    )
+    assert not [fp for fp in files if "default.go" in fp]
