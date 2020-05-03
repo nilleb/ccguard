cdcf14c6f04365df27e02919dd4d0f3f63de6f7f
Ivo Bellin Salarin <ivo@lumapps.com>
2020-05-03 17:14:47 +0200

    chore: refactor the distrib and release scripts

--- test_distrib/test_distrib.sh
+++ test_distrib/test_distrib.sh
@@ -1,12 +1,6 @@
-rm dist/*.whl
-rm -rf /tmp/venv
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+${DIR}/../common-distrib.sh
 
-PKG_NAME=$(python setup.py --name)
-PKG_VERSION=$(python setup.py --version)
-sed -i "" "s/\(__version__ = \"\).*\(\"\)/\1${PKG_VERSION}\2/g" ccguard/__init__.py
-
-pip install wheel
-python3 setup.py bdist_wheel
 python3 -m venv /tmp/venv
 source /tmp/venv/bin/activate
 cd /tmp/
